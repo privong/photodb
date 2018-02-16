08a166ffb35b928297cff1f2125b9d12dbc5aa88
George C. Privon <accounts5@privon.com>
2018-02-16 09:42:00 -0500

    description of initial idea and skeleton for some functions

---
+++ README.md
@@ -0,0 +1,10 @@
+# Photo Database
+
+Scan RAW images and store information into a database.
+Cross-correlate image timestamps with approximate [magellan](https://github.com/privong/magellan) GPS information to enable display/selection of images based on location.
+
+## Requirements
+
+Tested with python 3.6.
+
+- [rawkit](https://rawkit.readthedocs.io/en/latest/)
---
+++ photoDB.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+photoDB
+
+Scan directories of RAW photos and extract metadata. Commit said metadata
+to a database.
+"""
+
+
+import os
+import glob
+import sqlite3
+from rawkit.raw import Raw
+
+
+def getMetadata(imgpath):
+    """
+    Extract metadata from a file specified in the image path
+    """
+
+
+
+def main():
+    """
+    main routine
+    """
+
+
+
+if __name__ == "__main__":
+    main()
